6855dedc23cbec969225ba813358bb8ea22b9f2c
xz <502213050@qq.com>
2025-03-20 21:45:34 +0800

    Add files via upload

---
+++ sam/masking.py
@@ -0,0 +1,265 @@
+import argparse
+import os
+import sys
+
+import numpy as np
+import json
+import torch
+from PIL import Image
+
+sys.path.append(os.path.join(os.getcwd(), "GroundingDINO"))
+sys.path.append(os.path.join(os.getcwd(), "segment_anything"))
+import matplotlib
+matplotlib.use('Agg')  # 或者使用 'TkAgg', 'Qt5Agg' 等
+import matplotlib.pyplot as plt
+
+# Grounding DINO
+import GroundingDINO.groundingdino.datasets.transforms as T
+from GroundingDINO.groundingdino.models import build_model
+from GroundingDINO.groundingdino.util.slconfig import SLConfig
+from GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+
+
+
+
+# segment anything
+from segment_anything import (
+    sam_model_registry,
+    sam_hq_model_registry,
+    SamPredictor
+)
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_image(image_path):
+    # load image
+    image_pil = Image.open(image_path).convert("RGB")  # load image
+
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+    image, _ = transform(image_pil, None)  # 3, h, w
+    return image_pil, image
+
+
+def load_model(model_config_path, model_checkpoint_path, bert_base_uncased_path, device):
+    args = SLConfig.fromfile(model_config_path)
+    args.device = device
+    args.bert_base_uncased_path = bert_base_uncased_path
+    model = build_model(args)
+    checkpoint = torch.load(model_checkpoint_path, map_location="cpu")
+    load_res = model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
+    print(load_res)
+    _ = model.eval()
+    return model
+
+
+def get_grounding_output(model, image, caption, box_threshold, text_threshold, with_logits=True, device="cpu"):
+    caption = caption.lower()
+    caption = caption.strip()
+    if not caption.endswith("."):
+        caption = caption + "."
+    model = model.to(device)
+    image = image.to(device)
+    with torch.no_grad():
+        outputs = model(image[None], captions=[caption])
+    logits = outputs["pred_logits"].cpu().sigmoid()[0]  # (nq, 256)
+    boxes = outputs["pred_boxes"].cpu()[0]  # (nq, 4)
+    logits.shape[0]
+
+    # filter output
+    logits_filt = logits.clone()
+    boxes_filt = boxes.clone()
+    filt_mask = logits_filt.max(dim=1)[0] > box_threshold
+    logits_filt = logits_filt[filt_mask]  # num_filt, 256
+    boxes_filt = boxes_filt[filt_mask]  # num_filt, 4
+    logits_filt.shape[0]
+
+    # get phrase
+    tokenlizer = model.tokenizer
+    tokenized = tokenlizer(caption)
+    # build pred
+    pred_phrases = []
+    for logit, box in zip(logits_filt, boxes_filt):
+        pred_phrase = get_phrases_from_posmap(logit > text_threshold, tokenized, tokenlizer)
+        if with_logits:
+            pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
+        else:
+            pred_phrases.append(pred_phrase)
+
+    return boxes_filt, pred_phrases
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_box(box, ax, label):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+    ax.text(x0, y0, label)
+
+
+def save_mask_data(imgname, mask_list, box_list, label_list):
+    value = 0  # 0 for background
+
+    mask_img = torch.zeros(mask_list.shape[-2:])
+    for idx, mask in enumerate(mask_list):
+        mask_img[mask.cpu().numpy()[0] == True] = 255
+    mask_img = np.stack((mask_img,) * 3, axis=-1)
+    mask_img = mask_img.astype(np.uint8)  # 转换为 uint8 类型
+    mask_img = Image.fromarray(mask_img)
+
+
+import os
+
+def get_image_paths1(directory):
+    image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'}
+    image_paths = []
+
+    for filename in os.listdir(directory):
+        if filename.lower().endswith(tuple(image_extensions)):
+            image_paths.append(filename)
+
+    return image_paths
+
+# 使用示例
+def read_file_line_by_line(file_path):
+    id2clsname = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line.strip())
+            id2clsname[line.split(' ')[0]]=line.split(' ')[1][0:-1].replace('_', ' ')
+    return id2clsname
+            # 使用 strip() 去除行末的换行符
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("Grounded-Segment-Anything Demo", add_help=True)
+    parser.add_argument("--config", type=str, required=True, help="path to config file")
+    parser.add_argument(
+        "--grounded_checkpoint", type=str, required=True, help="path to checkpoint file"
+    )
+    parser.add_argument(
+        "--sam_version", type=str, default="vit_h", required=False, help="SAM ViT version: vit_b / vit_l / vit_h"
+    )
+    parser.add_argument(
+        "--sam_checkpoint", type=str, required=False, help="path to sam checkpoint file"
+    )
+    parser.add_argument(
+        "--sam_hq_checkpoint", type=str, default=None, help="path to sam-hq checkpoint file"
+    )
+    parser.add_argument(
+        "--use_sam_hq", action="store_true", help="using sam-hq for prediction"
+    )
+    parser.add_argument("--input_image", type=str, required=True, help="path to image file")
+    parser.add_argument("--text_prompt", type=str, required=True, help="text prompt")
+    parser.add_argument(
+        "--output_dir", "-o", type=str, default="outputs", required=True, help="output directory"
+    )
+
+    parser.add_argument("--box_threshold", type=float, default=0.3, help="box threshold")
+    parser.add_argument("--text_threshold", type=float, default=0.25, help="text threshold")
+    parser.add_argument("--root", type=str, default="")
+    parser.add_argument("--device", type=str, default="cpu", help="running on cpu only!, default=False")
+    parser.add_argument("--bert_base_uncased_path", type=str, required=False, help="bert_base_uncased model path, default=False")
+    args = parser.parse_args()
+
+    root = args.root
+    os.makedirs(os.path.join(root, 'mask'), exist_ok=True)
+    os.makedirs(os.path.join(root, 'mask_json'), exist_ok=True)
+
+    os.makedirs(os.path.join(root, 'inpaint'), exist_ok=True)
+    os.makedirs(os.path.join(root, 'score_inpaint'), exist_ok=True)
+    os.makedirs(os.path.join(root, 'texture'), exist_ok=True)
+    os.makedirs(os.path.join(root, 'visualization'), exist_ok=True)
+    os.makedirs(os.path.join(root, 'texture', 'top'), exist_ok=True)
+    os.makedirs(os.path.join(root, 'texture', 'low'), exist_ok=True)
+    # cfg
+    config_file = args.config  # change the path of the model config file
+    grounded_checkpoint = args.grounded_checkpoint  # change the path of the model
+    sam_version = args.sam_version
+    sam_checkpoint = args.sam_checkpoint
+    sam_hq_checkpoint = args.sam_hq_checkpoint
+    use_sam_hq = args.use_sam_hq
+    image_path = args.input_image
+    text_prompt = args.text_prompt
+    output_dir = args.output_dir
+    box_threshold = args.box_threshold
+    text_threshold = args.text_threshold
+    device = args.device
+    bert_base_uncased_path = args.bert_base_uncased_path
+
+    def get_image_paths(root_folder):
+        image_paths = []
+        # 遍历根文件夹下的所有子文件夹
+        for subdir, _, files in os.walk(root_folder):
+            for file in files:
+                # 检查文件扩展名是否是常见的图片格式
+                if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                    # 将完整路径添加到列表中
+                    image_paths.append(os.path.join(subdir, file))
+        return image_paths
+
+    directory_path = root + "/raw/"
+    images = get_image_paths(directory_path)
+    model = load_model(config_file, grounded_checkpoint, bert_base_uncased_path, device=device)
+    id2clsname = read_file_line_by_line("./imagenet/classnames.txt")
+    if use_sam_hq:
+        predictor = SamPredictor(sam_hq_model_registry[sam_version](checkpoint=sam_hq_checkpoint).to(device))
+    else:
+        predictor = SamPredictor(sam_model_registry[sam_version](checkpoint=sam_checkpoint).to(device))
+
+    directory_path1 = root + "/mask/"
+    images1 = get_image_paths1(directory_path1)
+
+    for image_path in images:
+        if image_path.split('/')[-1][0:-4]+'png' not in images1:
+            image_pil, image = load_image(image_path)
+            # load model
+            imgname = image_path.split('/')[-1]
+            img_clsid = imgname.split('_')[0]
+            img_clsname = id2clsname[img_clsid]
+
+            text_prompt = img_clsname
+            boxes_filt, pred_phrases = get_grounding_output(
+                model, image, text_prompt, box_threshold, text_threshold, device=device
+            )
+
+            image = cv2.imread(image_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            print(len(image.shape))
+            if len(image.shape) == 2:
+                image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+            predictor.set_image(image)
+
+            size = image_pil.size
+            H, W = size[1], size[0]
+            for i in range(boxes_filt.size(0)):
+                boxes_filt[i] = boxes_filt[i] * torch.Tensor([W, H, W, H])
+                boxes_filt[i][:2] -= boxes_filt[i][2:] / 2
+                boxes_filt[i][2:] += boxes_filt[i][:2]
+
+            boxes_filt = boxes_filt.cpu()
+            transformed_boxes = predictor.transform.apply_boxes_torch(boxes_filt, image.shape[:2]).to(device)
+
+            try:
+                masks, _, _ = predictor.predict_torch(
+                    point_coords = None,
+                    point_labels = None,
+                    boxes = transformed_boxes.to(device),
+                    multimask_output = False,
+                )
+            except:
+                continue
